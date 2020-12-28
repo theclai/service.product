e93307ddae07513cef1af4dfd59e4491c239ca8b
Faisal Rahman <faisal.rahman@live.com>
2020-12-28 17:57:27 +0800

    PG-74 update fake products

--- src/main/java/service/product/ProductServiceImpl.java
+++ src/main/java/service/product/ProductServiceImpl.java
@@ -7,6 +7,7 @@ package service.product;
 import AWS.ALBGrpc;
 import com.google.protobuf.Empty;
 import com.google.protobuf.Timestamp;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,17 +29,31 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
     private static final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
 
     @Override
-    public synchronized void getCategory(ID request, StreamObserver<Category> responseObserver) throws ParseException {
+    public synchronized void getCategory(ID request, StreamObserver<Category> responseObserver) {
 
         logger.debug("getCategory");
 
-        Date date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
         Instant time = date.toInstant();
 
         Timestamp transactionTime = Timestamp.newBuilder().setSeconds(time.getEpochSecond())
                 .setNanos(time.getNano()).build();
 
-        Date dateValid = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        Date dateValid = null;
+        try {
+            dateValid = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
         Instant instantValid = dateValid.toInstant();
 
         Timestamp validTime = Timestamp.newBuilder().setSeconds(instantValid.getEpochSecond())
@@ -61,16 +76,30 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
     }
 
     @Override
-    public synchronized void listCategories(ListCategoriesRequest request, StreamObserver<Categories> responseObserver) throws ParseException {
+    public synchronized void listCategories(ListCategoriesRequest request, StreamObserver<Categories> responseObserver)  {
 
         logger.debug("listCategories");
-        Date date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
         Instant time = date.toInstant();
 
         Timestamp transactionTime = Timestamp.newBuilder().setSeconds(time.getEpochSecond())
                 .setNanos(time.getNano()).build();
 
-        Date dateValid = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        Date dateValid = null;
+        try {
+            dateValid = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
         Instant instantValid = dateValid.toInstant();
 
         Timestamp validTime = Timestamp.newBuilder().setSeconds(instantValid.getEpochSecond())
@@ -134,7 +163,43 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
 
         logger.debug("getProduct");
 
-        Product product = Product.newBuilder().build();
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
+        Instant time = date.toInstant();
+
+        Timestamp transactionTime = Timestamp.newBuilder().setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano()).build();
+
+        Date dateValid = null;
+        try {
+            dateValid = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
+        Instant instantValid = dateValid.toInstant();
+
+        Timestamp validTime = Timestamp.newBuilder().setSeconds(instantValid.getEpochSecond())
+                .setNanos(instantValid.getNano()).build();
+
+        Product product = Product
+                .newBuilder()
+                .setId("//product.tapp/Product/9a0e4932-44be-11eb-b378-0242ac130002")
+                .setTransactionTime(transactionTime)
+                .setValidTime(validTime)
+                .setCreatedTime(transactionTime)
+                .setQuantity(10)
+                .setDefaultVariant(ProductVariant.newBuilder().build())
+                .setCategory("//product.tapp/Category/3dcd405f-d0b5-4841-b9f2-c1ef6394d989")
+                .build();
+
         responseObserver.onNext(product);
         responseObserver.onCompleted();
     }
@@ -144,7 +209,92 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
 
         logger.debug("listProducts");
 
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
+        Instant time = date.toInstant();
+
+        Timestamp transactionTime = Timestamp.newBuilder().setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano()).build();
+
+        Date dateValid = null;
+        try {
+            dateValid = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2020-12-28T02:46:18Z");
+        } catch (ParseException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription(e.getMessage())
+                    .asRuntimeException());
+        }
+        Instant instantValid = dateValid.toInstant();
+
+        Timestamp validTime = Timestamp.newBuilder().setSeconds(instantValid.getEpochSecond())
+                .setNanos(instantValid.getNano()).build();
+
+        Product product1 = Product
+                .newBuilder()
+                .setId("//product.tapp/Product/9a0e4932-44be-11eb-b378-0242ac130002")
+                .setTransactionTime(transactionTime)
+                .setValidTime(validTime)
+                .setCreatedTime(transactionTime)
+                .setQuantity(10)
+                .setDefaultVariant(ProductVariant.newBuilder().build())
+                .setCategory("//product.tapp/Category/3dcd405f-d0b5-4841-b9f2-c1ef6394d989")
+                .build();
+
+        Product product2 = Product
+                .newBuilder()
+                .setId("//product.tapp/Product/df659673-dc85-49bc-8af6-f7497275a064")
+                .setTransactionTime(transactionTime)
+                .setValidTime(validTime)
+                .setCreatedTime(transactionTime)
+                .setQuantity(0)
+                .setDefaultVariant(ProductVariant.newBuilder().build())
+                .setCategory("//product.tapp/Category/713ec0bc-7a85-4fb6-8f9d-e2a8837b1abd")
+                .build();
+
+        Product product3 = Product
+                .newBuilder()
+                .setId("//product.tapp/Product/19ef2f61-7ceb-4a69-a423-cdd513688e94")
+                .setTransactionTime(transactionTime)
+                .setValidTime(validTime)
+                .setCreatedTime(transactionTime)
+                .setQuantity(3)
+                .setDefaultVariant(ProductVariant.newBuilder().build())
+                .setCategory("//product.tapp/Category/3dcd405f-d0b5-4841-b9f2-c1ef6394d989")
+                .build();
+
+        Product product4 = Product
+                .newBuilder()
+                .setId("//product.tapp/Product/ec0cb10f-f275-4861-9d04-cde3c4f5b868")
+                .setTransactionTime(transactionTime)
+                .setValidTime(validTime)
+                .setCreatedTime(transactionTime)
+                .setQuantity(56)
+                .setDefaultVariant(ProductVariant.newBuilder().build())
+                .setCategory("//product.tapp/Category/3dcd405f-d0b5-4841-b9f2-c1ef6394d989")
+                .build();
+
+        Product product5 = Product
+                .newBuilder()
+                .setId("//product.tapp/Product/ec0cb10f-f275-4861-9d04-cde3c4f5b868")
+                .setTransactionTime(transactionTime)
+                .setValidTime(validTime)
+                .setCreatedTime(transactionTime)
+                .setQuantity(143)
+                .setDefaultVariant(ProductVariant.newBuilder().build())
+                .setCategory("//product.tapp/Category/713ec0bc-7a85-4fb6-8f9d-e2a8837b1abd")
+                .build();
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(product1);
+
         Products products = Products.newBuilder().build();
+
         responseObserver.onNext(products);
         responseObserver.onCompleted();
     }
