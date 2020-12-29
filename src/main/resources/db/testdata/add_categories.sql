9593430720b81a1ba32fa5fdad875bc66405188d
Faisal Rahman <faisal.rahman@live.com>
2020-12-29 13:28:02 +0800

    PG-73 fix sql

--- src/main/resources/db/testdata/add_categories.sql
+++ src/main/resources/db/testdata/add_categories.sql
@@ -1,17 +1,17 @@
 insert into category(title, subtitle, description, parent)
 values('Physical Goods','','Electronics and accessories delivered to your door','')
 
-insert into log(transaction_time)
-values('2020-12-28T02:46:18Z')
+insert into log(tx, transaction_time)
+values(0,CURRENT_TIMESTAMP)
 
 insert into category(title, subtitle, description, parent)
 values('Electronics','','','//product.tapp/Category/9a0e4932-44be-11eb-b378-0242ac130002')
 
-insert into log(transaction_time)
-values('2020-12-28T02:46:18Z')
+insert into log(tx, transaction_time)
+values(0,CURRENT_TIMESTAMP)
 
 insert into category(title, subtitle, description, parent)
 values('Phone Accessories','','','//product.tapp/Category/9a0e4932-44be-11eb-b378-0242ac130002')
 
-insert into log(transaction_time)
-values('2020-12-28T02:46:18Z')
+insert into log(tx, transaction_time)
+values(0,CURRENT_TIMESTAMP)
