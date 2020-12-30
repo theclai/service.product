cc2f4ad7cc120ef2014a46b024e7cf66e1b24129
Enrico de Klerk <enrico.de.klerk@tappmoney.com>
2020-12-30 11:57:23 +1100

    PG-94 Fix foreign key constraints

--- src/main/resources/db/migration/V1_002__create_product_variant_tables.sql
+++ src/main/resources/db/migration/V1_002__create_product_variant_tables.sql
@@ -1,6 +1,10 @@
 create table product_variant_tx (
-    like node_tx including indexes
+    id uuid primary key,
+    tx bigserial references log(tx),
+    created_time timestamp with time zone not null default (current_timestamp)
 );
+
+create index product_variant_created_time_tx_idx on product_variant_tx(created_time, id);
 
 create type product_money as (
     currency_code char(3),
@@ -8,10 +12,14 @@ create type product_money as (
     nanos int
 );
 
-create type product_form as enum ('digital', 'physical', 'billing', 'lending');
+create type product_variant_form as enum ('digital', 'physical', 'billing', 'lending');
 
 create table product_variant (
-    like node including indexes,
+    id uuid,
+    tx bigserial references log(tx),
+    primary key(id, tx),
+    valid_time timestamp with time zone not null default (current_timestamp),
+    deleted boolean default (false),
     title text not null,
     subtitle text,
     sku varchar(1024),
@@ -19,5 +27,8 @@ create table product_variant (
     product uuid references product_tx(id),
     quantity int not null,
     price product_money,
-    form product_form
+    form product_variant_form
 );
+
+create index product_variant_valid_time_id_tx_idx on product_variant(valid_time, id, tx);
+create index product_variant_id_valid_time_tx_idx on product_variant(id, valid_time, tx);
--- src/main/resources/db/migration/V1_004__create_option_tables.sql
+++ src/main/resources/db/migration/V1_004__create_option_tables.sql
@@ -1,10 +1,21 @@
 create table variant_option_tx (
-    like node_tx including indexes
+    id uuid primary key,
+    tx bigserial references log(tx),
+    created_time timestamp with time zone not null default (current_timestamp)
 );
 
+create index variant_option_tx_created_time_idx on variant_option_tx(created_time, id);
+
 create table variant_option (
-    like node including indexes,
+    id int,
+    tx bigserial references log(tx),
     variant uuid references product_variant_tx(id),
+    primary key(variant, id, tx),
+    valid_time timestamp with time zone not null default (current_timestamp),
+    deleted boolean default (false),
     key text not null,
     value text not null
 );
+
+create index variant_option_variant_id_valid_time_tx_idx on variant_option(variant, id, valid_time, tx);
+create index variant_option_valid_time_variant_id_tx_idx on variant_option(valid_time, variant, id, tx);
