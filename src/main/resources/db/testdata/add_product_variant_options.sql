4a08923aacf41f279f7fbc1f6dc4db63c96b21e5
Faisal Rahman <faisal.rahman@live.com>
2020-12-31 09:47:14 +0800

    PG-86 add product variant options test data

--- src/main/resources/db/testdata/add_product_variant_options.sql
+++ src/main/resources/db/testdata/add_product_variant_options.sql
@@ -1,8 +1,56 @@
-delete from variant_option;
 delete from variant_option_tx;
+delete from variant_option;
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('41f29159-5634-4e23-a600-59af937ee61f', 1, 0, current_timestamp);
 
 insert into variant_option(variant, id, tx, valid_time, value)
-values('41f29159-5634-4e23-a600-59af937ee61f','b733d139-a2f0-4f17-9f70-1edeb729aeb5', '0', current_timestamp, 'blue');
+values('41f29159-5634-4e23-a600-59af937ee61f', 1, '0', current_timestamp, 'Blue');
 
---insert into variant_option_tx(variant, id, tx)
---values('41f29159-5634-4e23-a600-59af937ee61f','b733d139-a2f0-4f17-9f70-1edeb729aeb5');
+insert into variant_option_tx(variant, id, tx, created_time)
+values('6aa5b22d-caa4-434c-a844-507d248d2a96', 2, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('6aa5b22d-caa4-434c-a844-507d248d2a96', 2, '0', current_timestamp, 'White');
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('023bf7d0-315b-4f4e-b202-0faf9b0bda2f', 3, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('023bf7d0-315b-4f4e-b202-0faf9b0bda2f', 3, '0', current_timestamp, 'Red');
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('3fa225ef-3b39-418c-8721-f3d8eb12973e', 4, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('3fa225ef-3b39-418c-8721-f3d8eb12973e', 4, '0', current_timestamp, 'Black');
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('44487274-c689-4596-a596-5dc3d71569e3', 5, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('44487274-c689-4596-a596-5dc3d71569e3', 5, '0', current_timestamp, 'Pink');
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('d5facab3-287a-4b45-98a9-ac55e00f932c', 6, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('d5facab3-287a-4b45-98a9-ac55e00f932c', 6, '0', current_timestamp, 'White');
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('53a6df9a-02e4-4d4d-b1f9-02300cfa3fc4', 7, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('53a6df9a-02e4-4d4d-b1f9-02300cfa3fc4', 7, '0', current_timestamp, 'Red');
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('d12f88b5-3668-4ab8-8dbc-3a64dc9ddb97', 8, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('d12f88b5-3668-4ab8-8dbc-3a64dc9ddb97', 8, '0', current_timestamp, 'Blue');
+
+insert into variant_option_tx(variant, id, tx, created_time)
+values('6816eb2d-96d4-4aa1-8604-f46340c2d222', 9, 0, current_timestamp);
+
+insert into variant_option(variant, id, tx, valid_time, value)
+values('6816eb2d-96d4-4aa1-8604-f46340c2d222', 9, '0', current_timestamp, 'Cartoon');
