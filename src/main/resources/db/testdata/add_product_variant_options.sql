c1c10ed72c85cdd2a5b6800aaaccb1f36fa35945
Enrico de Klerk <enrico.de.klerk@tappmoney.com>
2021-01-04 13:15:10 +1100

    PG-102 Update product variant property and options testdata

--- src/main/resources/db/testdata/add_product_variant_options.sql
+++ src/main/resources/db/testdata/add_product_variant_options.sql
@@ -1,56 +1,21 @@
-delete from variant_option_tx;
-delete from variant_option;
+insert into variant_option_tx(variant, id, tx) values
+    ('41f29159-5634-4e23-a600-59af937ee61f', 'colour', 0),
+    ('6aa5b22d-caa4-434c-a844-507d248d2a96', 'colour', 0),
+    ('023bf7d0-315b-4f4e-b202-0faf9b0bda2f', 'colour', 0),
+    ('3fa225ef-3b39-418c-8721-f3d8eb12973e', 'colour', 0),
+    ('44487274-c689-4596-a596-5dc3d71569e3', 'colour', 0),
+    ('d5facab3-287a-4b45-98a9-ac55e00f932c', 'colour', 0),
+    ('53a6df9a-02e4-4d4d-b1f9-02300cfa3fc4', 'colour', 0),
+    ('d12f88b5-3668-4ab8-8dbc-3a64dc9ddb97', 'colour', 0),
+    ('6816eb2d-96d4-4aa1-8604-f46340c2d222', 'colour', 0);
 
-insert into variant_option_tx(variant, id, tx, created_time)
-values('41f29159-5634-4e23-a600-59af937ee61f', 1, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('41f29159-5634-4e23-a600-59af937ee61f', 1, '0', current_timestamp, 'Blue');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('6aa5b22d-caa4-434c-a844-507d248d2a96', 2, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('6aa5b22d-caa4-434c-a844-507d248d2a96', 2, '0', current_timestamp, 'White');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('023bf7d0-315b-4f4e-b202-0faf9b0bda2f', 3, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('023bf7d0-315b-4f4e-b202-0faf9b0bda2f', 3, '0', current_timestamp, 'Red');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('3fa225ef-3b39-418c-8721-f3d8eb12973e', 4, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('3fa225ef-3b39-418c-8721-f3d8eb12973e', 4, '0', current_timestamp, 'Black');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('44487274-c689-4596-a596-5dc3d71569e3', 5, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('44487274-c689-4596-a596-5dc3d71569e3', 5, '0', current_timestamp, 'Pink');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('d5facab3-287a-4b45-98a9-ac55e00f932c', 6, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('d5facab3-287a-4b45-98a9-ac55e00f932c', 6, '0', current_timestamp, 'White');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('53a6df9a-02e4-4d4d-b1f9-02300cfa3fc4', 7, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('53a6df9a-02e4-4d4d-b1f9-02300cfa3fc4', 7, '0', current_timestamp, 'Red');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('d12f88b5-3668-4ab8-8dbc-3a64dc9ddb97', 8, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('d12f88b5-3668-4ab8-8dbc-3a64dc9ddb97', 8, '0', current_timestamp, 'Blue');
-
-insert into variant_option_tx(variant, id, tx, created_time)
-values('6816eb2d-96d4-4aa1-8604-f46340c2d222', 9, 0, current_timestamp);
-
-insert into variant_option(variant, id, tx, valid_time, value)
-values('6816eb2d-96d4-4aa1-8604-f46340c2d222', 9, '0', current_timestamp, 'Cartoon');
+insert into variant_option(variant, id, tx, value) values
+    ('41f29159-5634-4e23-a600-59af937ee61f', 'colour', 0, 'Blue'),
+    ('6aa5b22d-caa4-434c-a844-507d248d2a96', 'colour', 0, 'White'),
+    ('023bf7d0-315b-4f4e-b202-0faf9b0bda2f', 'colour', 0, 'Red'),
+    ('3fa225ef-3b39-418c-8721-f3d8eb12973e', 'colour', 0, 'Black'),
+    ('44487274-c689-4596-a596-5dc3d71569e3', 'colour', 0, 'Pink'),
+    ('d5facab3-287a-4b45-98a9-ac55e00f932c', 'colour', 0, 'White'),
+    ('53a6df9a-02e4-4d4d-b1f9-02300cfa3fc4', 'colour', 0, 'Red'),
+    ('d12f88b5-3668-4ab8-8dbc-3a64dc9ddb97', 'colour', 0, 'Blue'),
+    ('6816eb2d-96d4-4aa1-8604-f46340c2d222', 'colour', 0, 'Cartoon');
